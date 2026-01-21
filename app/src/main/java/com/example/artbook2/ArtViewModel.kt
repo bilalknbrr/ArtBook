ecdcbd20a901f1e4dafafee0ca9e87542f7dabbe
bilalknbrr <bilalknbr293@gmail.com>
2026-01-21 06:03:51 +0300

    Refactor: Migrate ViewModel to use Room DB instead of local list

--- app/src/main/java/com/example/artbook2/ArtViewModel.kt
+++ app/src/main/java/com/example/artbook2/ArtViewModel.kt
@@ -1,12 +1,34 @@
 package com.example.artbook2
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.room.Room
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
-class ArtViewModel: ViewModel() {
+class ArtViewModel(application: Application) : AndroidViewModel(application) {
 
-    val artList = mutableListOf<ArtModel>()
+    private val db = Room.databaseBuilder(
+        application,
+        ArtDataBase::class.java,
+        "art_database"
+    ).build()
 
-    fun addArt(art: ArtModel) {
-        artList.add(art)
+    private val dao = db.artDao()
+
+    val artlist : Flow<List<ArtModel>> = dao.getAllArts()
+
+    fun addArt(art: ArtModel){
+        viewModelScope.launch {
+            dao.insertArt(art)
+        }
     }
+
+    fun deleteArt(art: ArtModel){
+        viewModelScope.launch {
+            dao.deleteArt(art)
+        }
+    }
+
 }
